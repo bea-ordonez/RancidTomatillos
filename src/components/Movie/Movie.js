7e81a8fc9ea651600d0f9335b4f991e91f95b9d1
Travis Parks <travisparks0788@gmail.com>
2023-03-24 10:22:42 -0400

    Add all poster images from a data file

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react'
 import './Movie.css'
 
-const Movie = ({title, poster_path, background_path, average_rating, release_date, id}) => {
+const Movie = ({title, poster_path, id}) => {
   return (
       <div className='single-movie-card'>
-        <h3>{title}</h3>
-        <img className="poster-image" src={poster_path} alt={`${title} movie poster`} />
-        <img className="background-image" src={background_path} alt={`${title} movie poster`} />
-        <p>{average_rating}</p>
-        <p>{release_date}</p>
+        <img className="poster-image" id={id} key={id} src={poster_path} alt={`${title} movie poster`} />
+        {/* <img className="background-image" src={background_path} alt={`${title} movie poster`} /> */}
       </div>
   )
 }
