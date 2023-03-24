01efafe05023f9bee1df94e04a91adbe06a27254
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-03-24 15:57:24 -0600

    Add movie details to single movie

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -12,8 +12,15 @@ class SingleMovie extends Component {
         console.log('singleMovie',this.state.singleMovie)
         return (
           <div className="single-movie">  
+            <button>Home Page</button>
             <p>{this.state.singleMovie.title}</p>
             <img className="background-image" id={this.state.singleMovie.id} key={this.state.singleMovie.id} src={this.state.singleMovie.backdrop_path} alt={`${this.state.singleMovie.title} movie poster`} />
+            <p>Release Date: {this.state.singleMovie.release_date}</p>
+            <p>Summary:{this.state.singleMovie.overview}</p>
+            <p>Rating: {this.state.singleMovie.average_rating} TOMATOES</p>
+            <p>Category: {this.state.singleMovie.genres}</p>
+            <p>Runtime: {this.state.singleMovie.runtime}</p>
+            <p>{this.state.singleMovie.tagline}</p>
           </div>
         )
     }
