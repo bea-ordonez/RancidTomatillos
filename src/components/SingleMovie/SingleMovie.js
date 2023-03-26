6db9ef0f9e29e1f2042652492664bb3e908b42f7
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-03-26 10:50:02 -0600

    Refactorsingle movie class component into functional component

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,34 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './SingleMovie.css';
 import PropTypes from "prop-types";
 
-class SingleMovie extends Component {
-    constructor() {
-        super();
-        this.state = {}
-    }
-
-    render() {
-       const singleMov = this.props.singleMovie
-
-        return (
-          <div className="single-movie">  
-            <button onClick={() => this.props.showAllMovies()}>Home Page</button>
-            <p>{singleMov.title}</p>
-            <p>{singleMov.tagline}</p>
-            <img className="background-image" id={singleMov.id} key={singleMov.id} src={singleMov.backdrop_path} alt={`${singleMov.title} movie poster`} />
-            <p>Release Date: {singleMov.release_date}</p>
-            <p>Synopsis: {singleMov.overview}</p>
-            <p>Rating: {singleMov.average_rating} 🍅 </p>
-            <p>Genre: {singleMov.genres}</p>
-            <p>Runtime: {singleMov.runtime} minutes</p>
-            
-            
-          </div>
-        );
-    };
+const SingleMovie = ({singleMovie, showAllMovies}) => {
+  
+  return (
+    <div className="single-movie">  
+      <button onClick={() => showAllMovies()}>Home Page</button>
+      <p>{singleMovie.title}</p>
+      <p>{singleMovie.tagline}</p>
+      <img className="background-image" id={singleMovie.id} key={singleMovie.id} src={singleMovie.backdrop_path} alt={`${singleMovie.title} movie poster`} />
+      <p>Release Date: {singleMovie.release_date}</p>
+      <p>Synopsis: {singleMovie.overview}</p>
+      <p>Rating: {singleMovie.average_rating} 🍅 </p>
+      <p>Genre: {singleMovie.genres}</p>
+      <p>Runtime: {singleMovie.runtime} minutes</p>
+    </div>
+  )
 }
-
 
 export default SingleMovie;
 
@@ -36,3 +25,4 @@ SingleMovie.propTypes = {
   showAllMovies: PropTypes.func.isRequired,
   singleMovie: PropTypes.object.isRequired
 }
+
