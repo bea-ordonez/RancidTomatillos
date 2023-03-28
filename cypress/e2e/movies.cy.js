8b36e67155412b97777f87ae9fd58f8683384c2a
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-03-28 17:45:48 -0600

    Add cypress testing

--- cypress/e2e/movies.cy.js
+++ cypress/e2e/movies.cy.js
@@ -1,18 +1,17 @@
-describe('user shouuld see movie image for each movie in the movie container', () => {
+describe('user should see movie image for each movie in the movie container', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
-  })
+  });
 
   it('should connect to a local site', () => {
     cy.visit('http://localhost:3000')
-  })
+  });
 
   it('should see movie images', () => {
     cy.get('.poster-image').should('be.visible')
-  })
+  });
 
   it('should see a title of the movie cinema', () => {
     cy.contains('Bea & Travis\'s Movie Cinema')
-  })
-
+  });
 })
--- cypress/e2e/singleMovie.cy.js
+++ cypress/e2e/singleMovie.cy.js
@@ -1,5 +1,10 @@
-describe('', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+// describe('', () => {
+//   beforeEach(() => {
+//     cy.visit('http://localhost:3000/985939')
+//     cy.intercept(
+//       'GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/:movie_id'
+//     )
+//   });
+
+
+// })
