a9d405f36e98d679da8d2f67169c52922a975b33
Travis Parks <travisparks0788@gmail.com>
2023-03-28 10:27:09 -0400

    Add cypress package

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -2,20 +2,19 @@ import React from 'react';
 import './SingleMovie.css';
 import PropTypes from "prop-types";
 
-const SingleMovie = ({singleMovie, showAllMovies}) => {
-  
+const SingleMovie = ({singleMovie, showAllMovies, trailer}) => {
   return (
     <div className="single-movie" style={{backgroundImage:`url("${singleMovie.backdrop_path}")`}}>  
       <button className="single-movie-button" onClick={() => showAllMovies()}>Back To Home</button>
       <div className="movie-details">
         <h1>{singleMovie.title}</h1>
         <p>{singleMovie.tagline}</p>
-        {/* <img className="background-image" id={singleMovie.id} key={singleMovie.id} src={singleMovie.backdrop_path} alt={`${singleMovie.title} movie poster`} /> */}
         <p>Release Date: {singleMovie.release_date}</p>
         <p>Synopsis: {singleMovie.overview}</p>
         <p>Rating: {singleMovie.average_rating} 🍅 </p>
         <p>Genre: {singleMovie.genres}</p>
         <p>Runtime: {singleMovie.runtime} minutes</p>
+        <video src={trailer}></video>
       </div>
     </div>
   )
