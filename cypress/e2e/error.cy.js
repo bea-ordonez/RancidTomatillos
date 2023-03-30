aa5b41518211877f76bb77fcdede32c725c40b03
Travis Parks <travisparks0788@gmail.com>
2023-03-30 19:33:40 -0400

    Add error test

---
+++ cypress/e2e/error.cy.js
@@ -0,0 +1,18 @@
+describe("A user should see an error page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/436270/nope");
+  });
+
+  it("User should be redirect to an Error page if the URL is incorrect", () => {
+    cy.get('div[class="error-page"]').should("be.visible");
+  });
+
+  it('User should see the "Page Not Found" message', () => {
+    cy.get("h1").should("be.visible");
+  });
+
+  it('User should be redirected back to the home movie page when clicking the go home button', () => {
+    cy.contains('Go Home').click()
+    cy.url().should("include", "http://localhost:3000")
+  })
+})
