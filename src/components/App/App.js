f15f90cea505a3d8dbe7fd90224fb21d60c246cd
Travis Parks <travisparks0788@gmail.com>
2023-03-31 20:07:53 -0400

    Add switch to fix error

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,47 +5,39 @@ import SingleMovie from '../SingleMovie/SingleMovie.js';
 import Error from '../Error/Error';
 import fetchPromises from '../Api';
 import PropTypes from "prop-types";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Router } from 'react-router-dom';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       movies: [],
+      error: false
     };
   }
 
   componentDidMount = () => {
     fetchPromises("movies")
     .then((data) =>  this.setState({movies: data.movies}))
-    .catch((error) => this.setState( {error: "Something went wrong wrong"}));
+    .catch((error) => this.setState( {error: true}));
   }
 
-
   render() {
-    console.log('error' , this.state.singleMovie.id)
     return (
       <main>
-          <Route path="/:movieId" render={({match}) => <SingleMovie movieId={parseInt(match.params.movieId)} />} />
+          {!this.state.movies.length ?  <h1 className='loading-message'>Loading...</h1> : <h1 className="header-title">Bea &amp; Travis's Movie Cinema</h1> }
+        <Switch>
+          <Route path="/:movieId" render={({match}) => <SingleMovie movieId={(match.params.movieId)} />} />
           <Route exact path={"/"}>
-            {!this.state.movies.length ?  <h1 className='loading-message'>Loading...</h1> : <h1 className="header-title">Bea &amp; Travis's Movie Cinema</h1> }
-            <MovieContainer movies={this.state.movies} showSingleMovie={this.showSingleMovie} /> 
+            <MovieContainer movies={this.state.movies} /> 
           </Route>
           <Route path="*">
-            {/* {this.state.singleMovie.id == undefined && <Error />} */}
             <Error />
           </Route>
+        </Switch>
       </main>
     );
   }
 }
 
 export default App;
-
-App.propTypes = {
-  showAllMovies: PropTypes.func,
-  showSingleMovie: PropTypes.func,
-  singleMovie: PropTypes.object,
-  movies: PropTypes.array,
-  videos: PropTypes.arrayOf()
-}
--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -4,58 +4,61 @@ import PropTypes from "prop-types";
 import {Link}  from "react-router-dom";
 // import { render } from "@testing-library/react";
 import fetchPromises from '../Api';
+import Error from "../Error/Error";
 
 class SingleMovie extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       singleMovieSelected: null,
       singleMovieVideos: [],
-      singleMovieError: []
+      singleMovieError: [],
+      loading: true,
+      error: false
     }
   }
  
 
 componentDidMount = () => {
-  fetchPromises(`/movies/${this.props.MovieId}`)
-    .then((data) => this.setState({singleMovieVideos: data.movie}))
-    .catch((error) => this.setState( {error: "Something went wrong wrong"}))
+  fetchPromises(`movies/${this.props.movieId}`)
+    .then((data) => this.setState({singleMovieSelected: data.movie, loading: false}))
+    .catch((error) => this.setState( {error: true}))
 
-    fetchPromises(`/movies/${this.props.MovieId}/videos`)
-    .then((data) => this.setState({singleMovieTrailer: data.videos}))
-    .catch((error) => this.setState( {error: "Something went wrong wrong"}))
+    fetchPromises(`movies/${this.props.movieId}/videos`)
+    .then((data) => this.setState({singleMovieVideos: data.videos}))
+    .catch((error) => this.setState( {error: true}))
   }
   
   
-  componentDidUpdate = () => {
-    let movieKey
-    if(this.state.singleMovieVideos) {
-      const trailerMovie = this.singleMovieVideos.find((video) => video.type === "Trailer" && video.site === "YouTube")
-      movieKey = trailerMovie.key
+  getMovieTrailer = () => {
+    if(this.state.singleMovieVideos.length) {
+      const trailerMovie = this.state.singleMovieVideos.find((video) => video.type === "Trailer" && video.site === "YouTube")
+      return trailerMovie.key
     }
   }
   
 render() {
+
+  if(this.state.loading) {
+    return
+  } else {
   return (
     <div
     className="single-movie" style={{ backgroundImage: `url("${this.state.singleMovieSelected.backdrop_path}")` }} >
       <Link to="/">
       <button className="single-movie-button">Back To Home</button>
       </Link>
-      <Link to="/">
-      <h1 className="single-header-title">Bea &amp; Travis's Movie Cinema</h1>
-      </Link>
       <div className="movie-details">
-        <h1>Title: {this.props.singleMovieSelected.title}</h1>
-        <p>{this.props.singleMovieSelected.tagline}</p>
-        <p>Release Date: {this.props.singleMovieSelected.release_date}</p>
-        <p>Synopsis: {this.props.singleMovieSelected.overview}</p>
-        <p>Rating: {this.props.singleMovieSelected.average_rating}/ 10 🍅 </p>
-        <p>Genre: {this.props.singleMovieSelected.genres}</p>
-        <p>Runtime: {this.props.singleMovieSelected.runtime} minutes</p>
+        <h1>Title: {this.state.singleMovieSelected.title}</h1>
+        <p>{this.state.singleMovieSelected.tagline}</p>
+        <p>Release Date: {this.state.singleMovieSelected.release_date}</p>
+        <p>Synopsis: {this.state.singleMovieSelected.overview}</p>
+        <p>Rating: {this.state.singleMovieSelected.average_rating}/ 10 🍅 </p>
+        <p>Genre: {this.state.singleMovieSelected.genres}</p>
+        <p>Runtime: {this.state.singleMovieSelected.runtime} minutes</p>
         {this.state.singleMovieVideos.length && (
           <iframe
-          src={`https://www.youtube.com/embed/${movieKey}`}
+          src={`https://www.youtube.com/embed/${this.getMovieTrailer()}`}
           allowFullScreen
           title="video"
           width={500}
@@ -68,6 +71,7 @@ render() {
   );
 }
 }
+}
   
 
 
@@ -77,5 +81,5 @@ export default SingleMovie;
 
 
 SingleMovie.defaultProps = {
-  movieId: PropTypes.number.isRequired
+  movieId: PropTypes.string.isRequired
 }
