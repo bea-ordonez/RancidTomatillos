7c73049ebd14fcfd0b2de7ee6007135e1e15b7be
Travis Parks <travisparks0788@gmail.com>
2023-04-01 11:23:21 -0400

    error trial

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -3,12 +3,12 @@ import './Movie.css';
 import PropTypes from "prop-types";
 import { Link }  from "react-router-dom";
 
-const Movie = ({title, poster_path, id}) => {
+const Movie = ({title, poster_path, id, showSingleMovie}) => {
   return (
       
         <Link to={`/${id}`}>
           <div className='single-movie-card'>
-            <img className="poster-image"  id={id} key={id} src={poster_path} alt={`${title} movie poster`} />
+            <img className="poster-image" onClick={() => showSingleMovie(id)} id={id} key={id} src={poster_path} alt={`${title} movie poster`} />
           </div>
         </Link>
       
@@ -21,5 +21,6 @@ export default Movie;
 Movie.propTypes = {
   title: PropTypes.string,
   id: PropTypes.number.isRequired,
-  poster_path: PropTypes.string.isRequired
+  poster_path: PropTypes.string.isRequired,
+  showSingleMovie: PropTypes.func.isRequired
 }
